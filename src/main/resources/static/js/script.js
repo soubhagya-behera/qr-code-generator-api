26e40d48f70a9b6a380107d388bdea940a30e5e9
soubhagya-kumar-behera <soubhagyabehera074@gmail.com>
2026-06-16 00:04:38 +0530

    Finished QR Code Generator

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,12 +1,57 @@
-function generateQR(){
+function generateQR() {
 
-    alert("Frontend Working!");
+    let text =
+        document.getElementById(
+            "textInput"
+        ).value;
+
+    if(text.trim() === "") {
+
+        alert(
+            "Please enter text or URL"
+        );
+
+        return;
+    }
+
+    let qrUrl =
+        "/qr?text=" +
+        encodeURIComponent(text);
+
+    let qrImage =
+        document.getElementById(
+            "qrImage"
+        );
+
+    qrImage.src = qrUrl;
+
+    document
+        .getElementById("downloadBtn")
+        .href = qrUrl;
+
+    document
+        .getElementById("result")
+        .style.display = "block";
 }
 
-function clearQR(){
+function clearQR() {
 
-    document.getElementById(
-        "textInput"
-    ).value = "";
+    document
+        .getElementById("textInput")
+        .value = "";
 
+    document
+        .getElementById("result")
+        .style.display = "none";
 }
+
+document
+    .getElementById("textInput")
+    .addEventListener("keypress", function(event){
+
+        if(event.key === "Enter"){
+
+            generateQR();
+        }
+
+    });
